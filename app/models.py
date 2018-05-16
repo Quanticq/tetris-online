4eb35bfb6bc7725f452f3eb116f1f97a4e8edd8b
Quanticq <hisuai@Mail.ru>
2018-05-17 00:27:19 +0300

    db models edited

--- app/models.py
+++ app/models.py
@@ -4,18 +4,19 @@ from app import db
 
 tournaments_users = db.Table(
     "tournaments_users",
-    db.Column("tournament_id", db.Integer, db.ForeignKey("tournaments.id")),
-    db.Column("user_id", db.Integer, db.ForeignKey("users.id"))
+    db.Column("tournament_id", db.Integer, db.ForeignKey("tournaments.id"), primary_key=True),
+    db.Column("user_id", db.Integer, db.ForeignKey("users.id"), primary_key=True)
 )
 
 
 class User(db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), index=True, unique=True)
-    password_hash = db.Column(db.String(128))
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120), index=True, unique=True, nullable=False)
+    password_hash = db.Column(db.String(128), nullable=False)
     max_score = db.Column(db.Integer, default=0, index=True)
+    tournament = db.relationship("Tournament", secondary=tournaments_users, back_populates="member")
 
     def __repr__(self):
         return "<User {}>".format(self.name)
@@ -24,9 +25,10 @@ class User(db.Model):
 class Tournament(db.Model):
     __tablename__ = "tournaments"
 
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(120), index=True, unique=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(120), index=True, unique=True, nullable=False)
     description = db.Column(db.String(1024))
+    member = db.relationship("User", secondary=tournaments_users, back_populates="tournament")
 
     def __repr__(self):
         return "<Tournament {}>".format(self.name)
@@ -34,13 +36,14 @@ class Tournament(db.Model):
 
 class Fight(db.Model):
     __tablename__ = "fights"
-    id = db.Column(db.Integer, primary_key=True)
+
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     tournament_id = db.Column(db.Integer, db.ForeignKey("tournaments.id"))
     user1_id = db.Column(db.Integer, db.ForeignKey("users.id"))
     user2_id = db.Column(db.Integer, db.ForeignKey("users.id"))
-    user1_score = db.Column(db.Integer, default=0)
-    user2_score = db.Column(db.Integer, default=0)
-    status = db.Column(db.String(128), default="friendly")
+    user1_score = db.Column(db.Integer, default=0, nullable=False)
+    user2_score = db.Column(db.Integer, default=0, nullable=False)
+    status = db.Column(db.String(128), default="friendly", nullable=False)
 
     def __repr__(self):
         return '<Fight {0} vs. {1}>'.format(self.user1_id, self.user2_id)
