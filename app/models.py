124c92362b25142aff1d24225555cd72a3c340b0
Quanticq <hisuai@Mail.ru>
2018-05-19 21:21:53 +0300

    db model's methods added (in progress)

--- app/models.py
+++ app/models.py
@@ -1,6 +1,9 @@
 # -*- coding: utf-8 -*-
-from app import db
+from random import shuffle
 
+from werkzeug.security import generate_password_hash, check_password_hash
+
+from app import db
 
 tournaments_users = db.Table(
     "tournaments_users",
@@ -18,11 +21,22 @@ class User(db.Model):
     max_score = db.Column(db.Integer, default=0, index=True)
     tournaments = db.relationship("Tournament", secondary=tournaments_users, back_populates="members")
 
+    def __init__(self, name, password):
+        self.name = name
+        self.set_password(password)
+
     def __repr__(self):
         return "<User {}>".format(self.name)
 
     def set_score(self, value):
-        self.max_score = value
+        if value > self.max_score:
+            self.max_score = value
+
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+    def check_password(self, password):
+        return check_password_hash(self.password_hash, password)
 
 
 class Tournament(db.Model):
@@ -32,9 +46,35 @@ class Tournament(db.Model):
     name = db.Column(db.String(120), index=True, unique=True, nullable=False)
     description = db.Column(db.String(1024))
     members = db.relationship("User", secondary=tournaments_users, back_populates="tournaments")
+    fights = db.relationship("Fight", back_populates="tournament")
+
+    def __init__(self, name, description=None):
+        self.name = name
+        if description:
+            self.description = description
 
     def __repr__(self):
         return "<Tournament {}>".format(self.name)
+
+    def add_member(self, user):
+        self.members.append(user)
+
+    def delete_member(self, user):
+        self.members.remove(user)
+
+    def create_fights(self):
+        us1 = self.members[::2]
+        us2 = self.members[1::2]
+        shuffle(us1), shuffle(us2)
+
+        for pair in list(zip(us1, us2)):
+            self.fights.append(Fight(pair[0], pair[1], self.id, "1/{0}".format(len(us1))))
+
+    def delete_losers(self):
+        status = "1/{0}".format(len(self.members) // 2)
+        last_fights = [f for f in self.fights if f.status == status]
+        for fight in last_fights:
+            self.members.remove(fight.get_looser())
 
 
 class Fight(db.Model):
@@ -47,20 +87,33 @@ class Fight(db.Model):
     user1_score = db.Column(db.Integer, default=0, nullable=False)
     user2_score = db.Column(db.Integer, default=0, nullable=False)
     status = db.Column(db.String(128), default="friendly", nullable=False)
+    tournament = db.relationship("Tournament", back_populates="fights")
 
-    def __init__(self, u1: User, u2: User, tournament=None, status=None):
-        self.user1_id = u1.id
-        self.user2_id = u2.id
-        if tournament:
-            self.tournament_id = tournament.id
+    def __init__(self, user1, user2, tournament_id=0, status=None):
+        self.user1_id = user1.id
+        self.user2_id = user2.id
+        if tournament_id:
+            self.tournament_id = tournament_id
         if status:
             self.status = status
 
-    def set_score(self, u: User, value):
-        if u.id == self.user1_id:
+    def __repr__(self):
+        return '<Fight {0} vs. {1}>'.format(self.user1_id, self.user2_id)
+
+    def set_score(self, user, value):
+        if user.id == self.user1_id:
             self.user1_score = value
         else:
             self.user2_score = value
 
-    def __repr__(self):
-        return '<Fight {0} vs. {1}>'.format(self.user1_id, self.user2_id)
+    def get_looser(self):
+        if self.user1_score > self.user2_score:
+            return User.query.get(self.user2_id)
+        else:
+            return User.query.get(self.user1_id)
+
+    def get_winner(self):
+        if self.user1_score > self.user2_score:
+            return User.query.get(self.user1_id)
+        else:
+            return User.query.get(self.user2_id)
